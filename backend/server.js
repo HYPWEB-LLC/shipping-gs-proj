a7734747ad88238e023d3701849c5ac2c58f3875
Kartik Turak <kartikturak1@gmail.com>
2024-10-08 20:30:27 +0530

    Payment Integration Logic Added

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const adminRoutes = require('./routes/admin');
+const paymentRoutes = require('./routes/payment');
 
 const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // your frontend URL
-  credentials: true, // allow credentials
+  origin: 'http://localhost:5173', 
+  credentials: true, 
 };
 
+app.use(express.json({
+  verify: (req, _, buf) => {
+      req.rawBody = buf.toString();
+  }
+}))
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Use admin routes
 app.use('/api', adminRoutes);
-
+app.use('/api',paymentRoutes);
 // Default route
 app.get('/', (req, res) => {
     console.log("Default route");
@@ -27,3 +33,5 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
+
+
